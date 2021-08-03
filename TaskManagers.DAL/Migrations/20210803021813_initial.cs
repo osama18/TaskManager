8e18aff97c47fe44b106485c4514ad1de20fe081
osama.yassein@parkmobile.com <osama.yassein@parkmobile.com>
2021-08-03 04:29:54 +0200

    Create Migration

---
+++ TaskManagers.DAL/Migrations/20210803021813_initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskManager.DAL.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcessEntity",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Process_GroupName",
+                table: "ProcessEntity",
+                column: "GroupName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessEntity");
+        }
+    }
+}
